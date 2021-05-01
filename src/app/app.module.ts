46651d7337752a7b2e41d63b792f37df2f2cb607
pickettmap <mipi6515@colorado.edu>
2021-05-01 14:38:54 -0600

    added glassmorphism table styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ButtonComponent } from './global-components/button/button.component';
+import { TableComponent } from './global-components/table/table.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ButtonComponent
+    ButtonComponent,
+    TableComponent
   ],
   imports: [
     BrowserModule,
