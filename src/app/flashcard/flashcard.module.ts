93bd36f1b5a771951edb8a3c421507e2ca7e2447
Nick Z <nicolas.zimmerman@revature.net>
2021-05-01 11:03:02 -0600

    flashcart component

--- src/app/flashcard/flashcard.module.ts
+++ src/app/flashcard/flashcard.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FlashcardComponent } from './flashcard.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [FlashcardComponent],
   imports: [
     CommonModule
   ]
