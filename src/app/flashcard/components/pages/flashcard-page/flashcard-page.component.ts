f60c32cc5ecd4774332641767c01ca680e8c51d2
Luke Mohr <luke.mohr@revature.net>
2021-05-02 11:55:32 -0600

    Added flashcard component

---
+++ src/app/flashcard/components/pages/flashcard-page/flashcard-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-flashcard-page',
+  templateUrl: './flashcard-page.component.html',
+  styleUrls: ['./flashcard-page.component.css']
+})
+export class FlashcardPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
