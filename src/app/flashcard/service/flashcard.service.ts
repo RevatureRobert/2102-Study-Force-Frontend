5be80855a7d72e9859125a1974d33d4eb208597a
Luke Mohr <luke.mohr@revature.net>
2021-05-02 09:26:37 -0600

    Added flashcard service and model

---
+++ src/app/flashcard/service/flashcard.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@angular/core';
+import { HttpClient,HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Flashcard } from '../model/flashcard';
+
+const baseUrl = 'http://localhost:8080/flashcards';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FlashcardService {
+
+  constructor(private http: HttpClient) { }
+
+
+  getAll(): Observable<Flashcard[]> {
+    return this.http.get<Flashcard[]>(`${baseUrl}/all`);
+  }
+
+  getAllByDifficulty(difficulty: number): Observable<Flashcard[]> {
+    return this.http.get<Flashcard[]>(`${baseUrl}/difficulty?difficulty=${difficulty}`);
+  }
+
+  getAllByTopic(topic: string): Observable<Flashcard[]> {
+    return this.http.get<Flashcard[]>(`${baseUrl}/topic?topic=${topic}`);
+  }
+
+  getAllByResolved(resolved: boolean): Observable<Flashcard[]> {
+    return this.http.get<Flashcard[]>(`${baseUrl}/resolved?resolved=${resolved}`);
+  }
+
+  get(id: any): Observable<Flashcard> {
+    return this.http.get<Flashcard>(`${baseUrl}/id/${id}`);
+  }
+
+  create(data: any): Observable<any> {
+    return this.http.post(baseUrl, data);
+  }
+
+  update(data: any): Observable<any> {
+    return this.http.put(baseUrl, data);
+  }
+
+  delete(data: any): Observable<any> {
+    return this.http.delete(baseUrl, data);
+  }
+
+}
