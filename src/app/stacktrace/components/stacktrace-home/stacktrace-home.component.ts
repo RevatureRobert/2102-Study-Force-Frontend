486af77ff28be18f9a262c53d6ecc2cffef901fc
Nate Ford <nathaniel.ford@revature.net>
2021-05-03 10:46:09 -0500

    added filter by technology

--- src/app/stacktrace/components/stacktrace-home/stacktrace-home.component.ts
+++ src/app/stacktrace/components/stacktrace-home/stacktrace-home.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { StacktraceService } from '../../services/stacktrace.service';
 import { Stacktrace } from '../../models/stacktrace';
+import { TechnologyService } from '../../services/technology.service';
+import { Technology } from '../../models/technology';
 
 @Component({
   selector: 'app-stacktrace-home',
@@ -8,17 +10,44 @@ import { Stacktrace } from '../../models/stacktrace';
   styleUrls: ['./stacktrace-home.component.css']
 })
 export class StacktraceHomeComponent implements OnInit {
-  stacktraces: Stacktrace[] = [];
+  allStacktraces: Stacktrace[] = [];
+  filteredStacktraces: Stacktrace[] = [];
+  technologies: Technology[] = [];
   pageSize: number = 10;
   page: number = 1;
 
-  constructor(private stacktraceService: StacktraceService) { }
+  constructor(
+    private stacktraceService: StacktraceService,
+    private technologyService: TechnologyService
+    ) { }
 
   ngOnInit(): void {
-    this.getPageStacktrace();
+    this.getStacktrace();
+    this.getTechnology();
   }
 
-  getPageStacktrace(): void {
-    this.stacktraceService.getAllStacktrace();
+  filterStacktraces(technology?: Technology, searchParameter?: string): void {
+    if(technology) {
+      this.filteredStacktraces = this.allStacktraces.filter((stacktrace: Stacktrace) => {
+        return stacktrace.technology.technologyId = technology.technologyId;
+      })
+    } else {
+      this.filteredStacktraces = this.allStacktraces;
+    }
+  }
+
+  getStacktrace(): void {
+    this.stacktraceService.getAllStacktrace()
+      .then((data) => {
+        this.allStacktraces = data;
+        this.filterStacktraces();
+      });
+  }
+
+  getTechnology(): void {
+    this.technologyService.getAllTechnology()
+      .then((data) => {
+        this.technologies = data;
+      });
   }
 }
