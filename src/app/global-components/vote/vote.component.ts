1ed29401de0782b5ad01e9fbf510d127b5afc66c
pickettmap <mipi6515@colorado.edu>
2021-05-03 13:43:22 -0600

    vote component

--- src/app/global-components/vote/vote.component.ts
+++ src/app/global-components/vote/vote.component.ts
@@ -7,11 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VoteComponent implements OnInit {
 
-
+  upVoteSource = "../../../assets/up doot unclicked.svg"
+  downVoteSource = "../../../assets/down doot unclicked.svg"
+  scoreColor = "color: var(--duskwood)"
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  upVote() {
+    this.upVoteSource = "../../../assets/selected up doot.svg"
+    this.scoreColor = "color: var(--red-orange-juice)"
+  }
+
+  downVote() {
+    this.downVoteSource = "../../../assets/selected down doot.svg"
+    this.scoreColor = "color: var(--sapphire-splendor)"
   }
 
 }
