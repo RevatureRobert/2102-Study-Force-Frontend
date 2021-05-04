35c26630f21af90432853ab0115ca4451d888fef
dannyBernier <46949659+dannyBernier@users.noreply.github.com>
2021-05-04 16:01:55 -0500

    started work on user profile

--- src/app/user/user-routing.module.ts
+++ src/app/user/user-routing.module.ts
@@ -3,16 +3,18 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { RedirectGuardService } from './services/redirect-guard.service';
 import { BASE_URL_LOGIN_REGISTRATION } from '../../environments/environment';
+import { UserProfileComponent } from './components/user-profile/user-profile.component'
 
 const userRoutes: Routes = [
     {path: "login", component: LoginComponent},
     {
-        path: "loginRoute", 
-        canActivate: [RedirectGuardService], component: RedirectGuardService, 
+        path: "loginRoute",
+        canActivate: [RedirectGuardService], component: RedirectGuardService,
         data: {
             externalUrl: BASE_URL_LOGIN_REGISTRATION
         }
     },
+    {path: "profile", component: UserProfileComponent}
 ];
 
 @NgModule({
--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { LoginComponent } from './components/login/login.component';
 import { UserRoutingModule } from './user-routing.module';
 import { RedirectGuardService } from './services/redirect-guard.service';
+import { UserProfileComponent } from './components/user-profile/user-profile.component'
 
 @NgModule({
   declarations: [
     LoginComponent,
+    UserProfileComponent
     ],
 
   imports: [
