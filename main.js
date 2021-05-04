0da4ac270f7e97ceeac4d2eee5ac134e3f4f6d14
Ronald (Jeff) Lopez <32562609+lopezjronald@users.noreply.github.com>
2021-05-04 11:52:50 -0700

    added electron implementation to project

---
+++ main.js
@@ -0,0 +1,28 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+const url = require("url");
+
+let win;
+function createWindow() {
+  win = new BrowserWindow({ width: 1600, height: 1600 });
+  // load the dist folder from Angular
+  win.loadURL(
+    url.format({
+      pathname: path.join(__dirname, '/dist/index.html'), // compiled verion of our app
+      protocol: "file:",
+      slashes: true
+    })
+  );
+  // The following is optional and will open the DevTools:
+  // win.webContents.openDevTools()
+  win.on("closed", () => {
+    win = null;
+  });
+}
+app.on("ready", createWindow);
+// on macOS, closing the window doesn't quit the app
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
