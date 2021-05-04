2a87b64125c011419836f9396159b2a51c4e96d5
ely5 <ely5@case.edu>
2021-05-04 12:03:02 -0400

    init branch

---
+++ src/app/flashcard/components/ui/flashcard-answer/flashcard-answer.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Answer } from 'src/app/flashcard/model/answer';
+import { Flashcard } from 'src/app/flashcard/model/flashcard';
+
+@Component({
+  selector: 'app-flashcard-answer',
+  templateUrl: './flashcard-answer.component.html',
+  styleUrls: ['./flashcard-answer.component.css']
+})
+export class FlashcardAnswerComponent implements OnInit {
+  @Input() flashcard!: Flashcard;
+  @Input() answer!: Answer;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/flashcard/flashcard.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FlashcardComponent } from './flashcard.component';
+import { FlashcardPageComponent } from './components/pages/flashcard-page/flashcard-page.component';
+import { FlashcardQuestionComponent } from './components/ui/flashcard-question/flashcard-question.component';
+import { FlashcardAnswerComponent } from './components/ui/flashcard-answer/flashcard-answer.component';
+
+
+
+@NgModule({
+  declarations: [FlashcardComponent, FlashcardPageComponent, FlashcardQuestionComponent, FlashcardAnswerComponent],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    FlashcardPageComponent
+  ]
+})
+export class FlashcardModule { }
