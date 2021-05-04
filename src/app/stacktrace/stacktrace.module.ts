1e9e1ae1a273fe41411c842f4ae3432fa8b9949d
tkpanke <tpanke16@georgefox.edu>
2021-05-03 18:40:55 -0700

    changed styling from angular material to bootstrap

--- src/app/stacktrace/stacktrace.module.ts
+++ src/app/stacktrace/stacktrace.module.ts
@@ -2,22 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StacktraceHomeComponent } from './components/stacktrace-home/stacktrace-home.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { NewStacktraceComponent } from './components/new-stacktrace/new-stacktrace.component';
-import { MatCardModule } from '@angular/material/card';
-import { MatFormFieldModule } from '@angular/material/form-field';
 import { FormsModule } from '@angular/forms';
 
 
 
 @NgModule({
   declarations: [
-    StacktraceHomeComponent,
-    NewStacktraceComponent
+    StacktraceHomeComponent
   ],
   imports: [
     CommonModule,
-    MatCardModule,
-    MatFormFieldModule,
     FormsModule,
     NgbModule
   ]
