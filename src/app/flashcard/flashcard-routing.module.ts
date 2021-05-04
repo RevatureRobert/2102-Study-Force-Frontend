78a4c267a4d6acb1655534852d6cc02db17a23f2
Nick Z <nicolas.zimmerman@revature.net>
2021-05-03 22:05:32 -0600

    merging

--- src/app/flashcard/flashcard-routing.module.ts
+++ src/app/flashcard/flashcard-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FlashcardPageComponent } from './components/pages/flashcard-page/flashcard-page.component';
 import { FlashcardComponent } from './flashcard.component'
 
 
-const routes: Routes = [{ path: '', component: FlashcardComponent }];
+const routes: Routes = [
+  { path: '', component: FlashcardComponent },
+  { path: '/all', component: FlashcardPageComponent}
+];
 
 @NgModule({
 
