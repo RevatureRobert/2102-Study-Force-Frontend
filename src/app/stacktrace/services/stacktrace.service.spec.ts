223a3f9f8f4679bb1d7dc2a1461a382bd7ad0d7f
Nate Ford <nathaniel.ford@revature.net>
2021-05-05 14:14:52 -0500

    unstage tesing changes

--- src/app/stacktrace/services/stacktrace.service.spec.ts
+++ src/app/stacktrace/services/stacktrace.service.spec.ts
@@ -1,49 +1,16 @@
-import { TestBed, getTestBed } from '@angular/core/testing';
-import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { TestBed } from '@angular/core/testing';
 
-//import { BASE_API_URL} from 'src/environments/environment';
 import { StacktraceService } from './stacktrace.service';
-import { Stacktrace } from '../models/stacktrace';
 
 describe('StacktraceService', () => {
-  let injector: TestBed;
   let service: StacktraceService;
-  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule],
-      providers: [StacktraceService]
-    });
-    injector = getTestBed();
-    service = injector.inject(StacktraceService);
-    httpMock = injector.inject(HttpTestingController);
-  });
-
-  afterEach(() => {
-    httpMock.verify();
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(StacktraceService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
-  });
-
-  it('getAllStacktrace should return value from a promise', () => {
-    const dummyStacktraces: Stacktrace[] = [
-      { title: 'John',
-        body: 'Fron' },
-      { title: 'Doe',
-        body: 'Joe'}
-    ];
-
-    service.getAllStacktrace().then(value => {
-      expect(value).toEqual(dummyStacktraces);
-    });
-
-
-    let apiServerUrl = "http://localhost:8080"; //BASE_API_URL;
-    const req = httpMock.expectOne(`${service.apiServerUrl}/stacktrace`);
-    expect(req.request.method).toBe("GET");
-    req.flush(dummyStacktraces);
   });
 });
