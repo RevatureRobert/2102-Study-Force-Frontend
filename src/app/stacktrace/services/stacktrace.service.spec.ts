395ff70c018c63fd399da485dc218ce0e42fbe70
Nate Ford <nathaniel.ford@revature.net>
2021-05-05 14:11:55 -0500

    more service work

--- src/app/stacktrace/services/stacktrace.service.spec.ts
+++ src/app/stacktrace/services/stacktrace.service.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed, getTestBed } from '@angular/core/testing';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
+//import { BASE_API_URL} from 'src/environments/environment';
 import { StacktraceService } from './stacktrace.service';
 import { Stacktrace } from '../models/stacktrace';
 
@@ -27,18 +28,22 @@ describe('StacktraceService', () => {
     expect(service).toBeTruthy();
   });
 
-  it('getAllStacktrace should return value from a promise',
+  it('getAllStacktrace should return value from a promise', () => {
     const dummyStacktraces: Stacktrace[] = [
       { title: 'John',
         body: 'Fron' },
       { title: 'Doe',
-         }
+        body: 'Joe'}
     ];
 
-    (done: DoneFn) => {
     service.getAllStacktrace().then(value => {
-      expect(value).toBe('promise value');
-      done();
+      expect(value).toEqual(dummyStacktraces);
     });
+
+
+    let apiServerUrl = "http://localhost:8080"; //BASE_API_URL;
+    const req = httpMock.expectOne(`${service.apiServerUrl}/stacktrace`);
+    expect(req.request.method).toBe("GET");
+    req.flush(dummyStacktraces);
   });
 });
