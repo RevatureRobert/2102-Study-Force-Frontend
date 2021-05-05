7de34385a7f7d012b2d5658562c270e8d5bea8a7
Nate Ford <nathaniel.ford@revature.net>
2021-05-05 13:52:07 -0500

    in progress service test

--- src/app/stacktrace/services/stacktrace.service.spec.ts
+++ src/app/stacktrace/services/stacktrace.service.spec.ts
@@ -1,16 +1,36 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, getTestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 import { StacktraceService } from './stacktrace.service';
 
 describe('StacktraceService', () => {
+  let injector: TestBed;
   let service: StacktraceService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(StacktraceService);
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [StacktraceService]
+    });
+    injector = getTestBed();
+    service = injector.inject(StacktraceService);
+    httpMock = injector.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('getAllStacktrace should return value from a promise',
+    (done: DoneFn) => {
+    service.getAllStacktrace().then(value => {
+      expect(value).toBe('promise value');
+      done();
+    });
   });
 });
