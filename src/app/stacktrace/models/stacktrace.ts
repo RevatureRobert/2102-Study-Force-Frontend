bf9e9f75bdc9eb723705a606d6e522256f9f6a5a
Nate Ford <nathaniel.ford@revature.net>
2021-05-05 16:38:09 -0500

    Updated models to reflect updates in backend

--- src/app/stacktrace/models/stacktrace.ts
+++ src/app/stacktrace/models/stacktrace.ts
@@ -1,8 +1,9 @@
 import { Technology } from "./technology";
+import { StacktraceUser } from "./user";
 
 export interface Stacktrace {
   stacktraceId?: number;
-  // creator: User;
+  creator: StacktraceUser;
   title: string;
   body: string;
   technology?: Technology;
