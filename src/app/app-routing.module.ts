b8f1f436fe854fcedb53aad079028ed274690f25
EdsonR93 <edson.rod.p19@gmail.com>
2021-05-06 14:25:28 -0700

    adding home routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home-component/home/home.component';
 
 const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
   {
     path: 'flashcards',
     loadChildren: () =>
