836cb90cdaa6ca1060a7256ccffecdd0339d65fa
Sam Daniel <SamDanielEngineering@Gmail.com>
2021-05-06 17:45:03 -0400

    adding page base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,7 @@ import { PaginationComponent } from './global-components/pagination/pagination.c
 import { DiologueCardComponent } from './global-components/diologue-card/diologue-card.component';
 import { HomeComponent } from './home-component/home/home.component';
 import { ListComponent } from './global-components/list/list.component';
+import { BasePageComponent } from './global-components/base-page/base-page.component';
 
 @NgModule({
   declarations: [
@@ -40,6 +41,7 @@ import { ListComponent } from './global-components/list/list.component';
     DiologueCardComponent,
     HomeComponent,
     ListComponent,
+    BasePageComponent,
   ],
   imports: [BrowserModule, AppRoutingModule, NgbModule],
   providers: [],
