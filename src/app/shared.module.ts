b182a9210dcab81b51502d3ceea697de421138c7
pickettmap <mipi6515@colorado.edu>
2021-05-07 01:27:15 -0600

    Styled Flashcard page view

--- src/app/shared.module.ts
+++ src/app/shared.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AppModule } from './app.module';
 import { BasePageComponent } from "./global-components/base-page/base-page.component";
 import { NavbarComponent } from "./global-components/navbar/navbar.component";
 import { SearchBarComponent } from './global-components/search-bar/search-bar.component';
