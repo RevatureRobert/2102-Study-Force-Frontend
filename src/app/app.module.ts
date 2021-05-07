0362f79fa1784ae2e7c69874c9611253433dca85
Nate Ford <nathaniel.ford@revature.net>
2021-05-07 13:34:17 -0500

    use dev app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,38 +6,29 @@ import { SharedModule } from './global-components/shared.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-<<<<<<< HEAD
 import {FlashcardModule } from './flashcard/flashcard.module';
 import { FormsModule } from '@angular/forms';
 import { StacktraceModule } from './stacktrace/stacktrace.module';
 import { UserModule } from './user/user.module';
 
-=======
-import { FlashcardModule } from './flashcard/flashcard.module';
-import { StacktraceModule } from './stacktrace/stacktrace.module';
-import { UserModule } from './user/user.module'
->>>>>>> 64be4cd3bd36ffe82403bee85dc7b9a6f5dee6e1
-
-
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ButtonComponent,
+    GlassPaneComponent,
+    GenericCardWideComponent,
+    GenericCardComponent,
+    SearchBarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-<<<<<<< HEAD
     HttpClientModule,
     FormsModule,
     NgbModule,
     SharedModule,
     FlashcardModule,
     StacktraceModule,
-=======
-    NgbModule,
-    FlashcardModule,
-    StacktraceModule
->>>>>>> 64be4cd3bd36ffe82403bee85dc7b9a6f5dee6e1
     UserModule
   ],
   providers: [],
