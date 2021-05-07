17f693f618e0ee5f5829f3076859d282abdd35f7
Nate Ford <nathaniel.ford@revature.net>
2021-05-07 13:31:41 -0500

    use dev version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { SharedModule } from './global-components/shared.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import {FlashcardModule } from './flashcard/flashcard.module';
+import { FormsModule } from '@angular/forms';
+import { StacktraceModule } from './stacktrace/stacktrace.module';
+import { UserModule } from './user/user.module';
+
+
 
 @NgModule({
   declarations: [
@@ -12,9 +20,15 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    HttpClientModule,
+    FormsModule,
+    NgbModule,
+    SharedModule,
+    FlashcardModule,
+    StacktraceModule,
+    UserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
