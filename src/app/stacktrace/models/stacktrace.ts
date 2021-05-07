a593f674e1882d26e66d76453d0737db981ee5ae
Jl3E <joey.elmblad@gmail.com>
2021-05-07 16:41:10 -0700

    updated models so the application would actually start

--- src/app/stacktrace/models/stacktrace.ts
+++ src/app/stacktrace/models/stacktrace.ts
@@ -1,10 +1,12 @@
+import { Technology } from "./technology";
+
 export interface Stacktrace {
   stacktraceId: number;
   userId: number;
   userName: string;
   title: string;
   body: string;
-  technologyId: number;
+  technology: Technology;
   technologyName: string;
   creationTime: string;
 }
