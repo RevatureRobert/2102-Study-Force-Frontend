ecaf80d99e86ba98470e75f80c52238689f40a63
Sam Daniel <SamDanielEngineering@Gmail.com>
2021-05-07 15:45:17 -0400

    fixing home route

--- src/app/stacktrace/stacktrace-routing.module.ts
+++ src/app/stacktrace/stacktrace-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   /*{
-    path: '/stacktrace', component: StacktraceHomeComponent
+    path: '', component: StacktraceHomeComponent
   }
   {
     path: '/new-stacktrace', component: NewStacktraceComponent
