10cdf4a9aeaf76bfbf1932995caa7bb796398ac3
EdsonR93 <edson.rod.p19@gmail.com>
2021-05-07 14:27:47 -0700

    added missing router implementation on submit answer

--- src/app/flashcard/components/pages/submit-answer-page/submit-answer-page.component.ts
+++ src/app/flashcard/components/pages/submit-answer-page/submit-answer-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Flashcard } from 'src/app/flashcard/model/flashcard';
 import { AnswerService } from 'src/app/flashcard/service/answer.service';
 import { FlashcardService } from 'src/app/flashcard/service/flashcard.service';
@@ -15,8 +16,9 @@ export class SubmitAnswerPageComponent implements OnInit {
   };
   private userId:number = 1;
   flashcard!:Flashcard;
+  flashcardDifficulty?:string;
 
-  constructor(private answerService: AnswerService, private flashcardService: FlashcardService) {
+  constructor(private answerService: AnswerService, private flashcardService: FlashcardService, private router: Router) {
 
   }
 
@@ -25,6 +27,21 @@ export class SubmitAnswerPageComponent implements OnInit {
     this.flashcardService.getFlashcardById(flashcardId).then(response =>{
       console.log(response);
       this.flashcard = response;
+
+      switch (this.flashcard.difficulty) {
+        case 0:
+            this.flashcardDifficulty = "Easy";
+          break;
+        case 1:
+            this.flashcardDifficulty = "Medium";
+          break;
+        case 2:
+            this.flashcardDifficulty = "Hard";
+          break;
+        default:
+          break;
+      }
+
     }).catch(exception =>{
       console.log(exception);
     })
@@ -39,6 +56,7 @@ export class SubmitAnswerPageComponent implements OnInit {
     console.log(this.form.answer);
     this.answerService.postAnswer(this.flashcard.flashcardId,this.userId,this.form.answer).then(res =>{
       console.log(res);
+      this.router.navigate(["/view-flashcards"]);
     }).catch(error => {
       console.log(error);
     })
