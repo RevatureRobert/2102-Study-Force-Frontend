ecf7591463e13a15b3dd1a192c82b1addf6b92ec
pickettmap <mipi6515@colorado.edu>
2021-05-07 08:17:33 -0600

    Adding in Shared Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,61 +1,57 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { SharedModule } from '../app/shared.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { TableComponent } from './global-components/table/table.component';
-import { NavbarComponent } from './global-components/navbar/navbar.component';
 import { FlashcardQuestionComponent } from './global-components/flashcard-question/flashcard-question.component';
 import { ReplyCardComponent } from './global-components/reply-card/reply-card.component';
-import { VoteComponent } from './global-components/vote/vote.component';
 import { SubmitContentCardComponent } from './global-components/submit-content-card/submit-content-card.component';
 import { FlashcardAnswerComponent } from './global-components/flashcard-answer/flashcard-answer.component';
 import { GlassPaneComponent } from './global-components/glass-pane/glass-pane.component';
 import { GenericCardWideComponent } from './global-components/generic-card-wide/generic-card-wide.component';
 import { GenericCardComponent } from './global-components/generic-card/generic-card.component';
-import { SearchBarComponent } from './global-components/search-bar/search-bar.component';
 import { DropdownComponent } from './global-components/dropdown/dropdown.component';
 import { PaginationComponent } from './global-components/pagination/pagination.component';
 import { DiologueCardComponent } from './global-components/diologue-card/diologue-card.component';
 import { HomeComponent } from './home-component/home/home.component';
 import { ListComponent } from './global-components/list/list.component';
-import { BasePageComponent } from './global-components/base-page/base-page.component';
-
-// import { FlashcardModule } from './flashcard/flashcard.module';
+import {FlashcardModule } from './flashcard/flashcard.module';
 import { FormsModule } from '@angular/forms';
-// import { StacktraceModule } from './stacktrace/stacktrace.module';
+import { StacktraceModule } from './stacktrace/stacktrace.module';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     TableComponent,
-    NavbarComponent,
     FlashcardQuestionComponent,
     ReplyCardComponent,
-    VoteComponent,
     SubmitContentCardComponent,
     FlashcardAnswerComponent,
     GlassPaneComponent,
     GenericCardWideComponent,
     GenericCardComponent,
-    SearchBarComponent,
     DropdownComponent,
     PaginationComponent,
     DiologueCardComponent,
     HomeComponent,
     ListComponent,
-    BasePageComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule,
     HttpClientModule,
-    // FlashcardModule,
     FormsModule,
-    // StacktraceModule,
+    NgbModule,
+    SharedModule,
+    FlashcardModule,
+    StacktraceModule
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/stacktrace/stacktrace.module.ts
+++ src/app/stacktrace/stacktrace.module.ts
@@ -1,18 +1,14 @@
-// import { NgModule } from '@angular/core';
-// import { CommonModule } from '@angular/common';
-// import { StacktraceHomeComponent } from './components/stacktrace-home/stacktrace-home.component';
-// import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-// import { StacktraceRoutingModule } from './stacktrace-routing.module';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
-// @NgModule({
-//   declarations: [StacktraceHomeComponent],
-//   imports: [
-//     CommonModule,
-//     NgbModule,
-//     StacktraceRoutingModule
-//   ],
-//   exports: [
-//     StacktraceHomeComponent
-//   ]
-// })
-// export class StacktraceModule { }
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    NgbModule
+  ],
+  exports: [
+  ]
+})
+export class StacktraceModule { }
