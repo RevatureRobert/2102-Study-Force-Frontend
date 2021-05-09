17ba072d31c493b7d96c6e9156593575af3df149
Luke Mohr <luke.mohr@revature.net>
2021-05-09 12:16:03 -0600

    added tests (mostly 'should be created')

--- src/app/global-components/vote/vote.component.ts
+++ src/app/global-components/vote/vote.component.ts
@@ -15,7 +15,6 @@ export class VoteComponent implements OnInit {
   @Input() answer!: Answer;
   @Input() answerId!: number;
   answerScore: number = 0;
-  TEST = "DEFAULT";
   @Input() userId!: number;
 
   upVoteSource = "../../../assets/up doot unclicked.svg"
