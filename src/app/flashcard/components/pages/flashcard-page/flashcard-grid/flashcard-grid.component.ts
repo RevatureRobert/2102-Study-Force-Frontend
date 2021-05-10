a01873d19e99f0602da3cb1f908ec5fe51d81a13
Kevin <kwang44@buffalo.edu>
2021-05-10 14:27:59 -0400

    dropdown style fix

--- src/app/flashcard/components/pages/flashcard-page/flashcard-grid/flashcard-grid.component.ts
+++ src/app/flashcard/components/pages/flashcard-page/flashcard-grid/flashcard-grid.component.ts
@@ -152,6 +152,46 @@ export class FlashcardGridComponent implements OnInit {
     }
   }
 
+  yes:boolean = false;
+
+  changeFocus() {
+    let parent = document.getElementById('Dropdown-Button');
+
+    if (this.yes === false) {
+      this.yes = true;
+      parent!.style.setProperty('border-bottom-right-radius', '0px');
+      parent!.style.setProperty('border-bottom-left-radius', '0px');
+    } else {
+      this.yes = false;
+      parent!.style.setProperty('border-bottom-right-radius', '10px');
+      parent!.style.setProperty('border-bottom-left-radius', '10px');
+    }
+  }
+
+  setFalse() {
+    this.yes = false;
+  }
+
+  yes2:boolean = false;
+
+  changeFocus2() {
+    let parent = document.getElementById('Dropdown-Button2');
+
+    if (this.yes2 === false) {
+      this.yes2 = true;
+      parent!.style.setProperty('border-bottom-right-radius', '0px');
+      parent!.style.setProperty('border-bottom-left-radius', '0px');
+    } else {
+      this.yes2 = false;
+      parent!.style.setProperty('border-bottom-right-radius', '10px');
+      parent!.style.setProperty('border-bottom-left-radius', '10px');
+    }
+  }
+
+  setFalse2() {
+    this.yes2 = false;
+  }
+
 }
 
 interface DisplayData {
