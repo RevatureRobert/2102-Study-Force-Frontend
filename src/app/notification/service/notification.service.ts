8651cfe7e1b66fac4d4b2119e470096fa65ec95a
pgonzalez017 <42748202+pgonzalez017@users.noreply.github.com>
2021-05-10 18:05:16 -0400

    Added New Notification Stuff

--- src/app/notification/service/notification.service.ts
+++ src/app/notification/service/notification.service.ts
@@ -1,9 +1,7 @@
 import {HttpClient} from '@angular/common/http';
 import {Injectable} from '@angular/core';
 import {Notification} from '../model/notification';
-import {map} from 'rxjs/operators';
 import {Observable} from 'rxjs';
-
 
 
 @Injectable({
@@ -11,22 +9,46 @@ import {Observable} from 'rxjs';
 })
 export class NotificationService {
 
+  notifications!: Notification[];
+  private headerInfo = {
+    'Content-Type': 'application/json',
+    Accept: 'application/json',
+  };
   private baseUrl = 'http://localhost:8080/notifications';
 
+  /*
+@param httpClient
+Injecting the HttpClient object to notificationService
+Author: Ronald Lopez
+*/
   constructor(private httpClient: HttpClient) {
   }
 
-  // tslint:disable-next-line:typedef
-  getNotifications(): Observable<Notification[]> {
-      return this.httpClient.get<GetResponseNotifications>(this.baseUrl).pipe(
-        map(response => response._embedded.notifications)
-    );
+  /*
+  returns all notifications endpoint from the backend based on user id
+  Author: Ronald Lopez
+   */
+  getAllNotifications(): Observable<any> {
+    return this.httpClient.get(`${(this.baseUrl)}/users/${1}`);
   }
 
-}
+  /*
+  @param id: number
+  @return httpClient to delete a notification by id
+  Deletes a notification endpoint at the backend based on notification id
+  Author: Ronald Lopez
+ */
+  deleteByNotificationId(id: number): Observable<any> {
+    return this.httpClient.delete(`${(this.baseUrl)}/${id}`);
+  }
 
-interface GetResponseNotifications {
-  _embedded: {
-    notifications: Notification[];
-  };
+  /*
+  @param id: number
+  @return httpClient to delete a notification by id
+  Deletes all notifications based on user id
+  Author: Ronald Lopez
+ */
+  deleteAllNotificationsByUserId(id: number): Observable<any> {
+    return this.httpClient.delete(`${(this.baseUrl)}/users/${id}`);
+  }
 }
