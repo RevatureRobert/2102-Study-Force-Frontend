44591137fcf388ccf2d4846b2f6e1c984c7ad6a2
Nick <nickmanpr@gmail.com>
2021-05-11 15:17:20 -0600

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://studyforce.auth.us-east-1.amazoncognito.com/login?client_id=1v2vmtuo6oiaace3vckil4cfqf&response_type=token&scope=aws.cognito.signin.user.admin&redirect_uri=http://localhost:4200/oauth2/'
+  apiUrl: 'https://52.91.175.61'
 };
