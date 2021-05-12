aca9cfaf2e08681f0b3b39f24d7c428abe56fae2
yahshua <yahshua@yamm.local>
2021-05-11 18:40:34 -0600

    security stuff

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'https://52.91.175.61'
+  apiUrl: 'http://52.91.175.61'
 };
 
 /*
@@ -17,4 +17,4 @@ export const environment = {
 // import 'zone.js/dist/zone-error';  // Included with Angular CLI.
 
 // Base URL for Hosted UI for login and signup
-export const BASE_URL_LOGIN_REGISTRATION = 'https://studyforce.auth.us-east-1.amazoncognito.com/login?client_id=1v2vmtuo6oiaace3vckil4cfqf&response_type=token&scope=aws.cognito.signin.user.admin&redirect_uri=http://revature.studyforce.it.s3-website-us-east-1.amazonaws.com/oauth2/';
+export const BASE_URL_LOGIN_REGISTRATION = 'https://studyforce.auth.us-east-1.amazoncognito.com/login?client_id=1v2vmtuo6oiaace3vckil4cfqf&response_type=token&scope=aws.cognito.signin.user.admin&redirect_uri=https://revature.studyforce.it.s3-website-us-east-1.amazonaws.com/oauth2/';
